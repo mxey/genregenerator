4f6e17f2dac0f5a4843505a71d506c170f1063a4
Stefan Ritter <xeno@thehappy.de>
2011-05-26 16:05:04 +0200

    * Add cities * Add html templates for planned website

--- genregenerator.py
+++ genregenerator.py
@@ -12,11 +12,21 @@
 
 from random import choice, randint
 
-subgroup = ["medieval", "death", "black", "hard", "progressive", "symphonic", "viking", "pagan", "gangsta", "experimental", "alternative", "minimal", "dark", "synth", "future", "body", "christian", "indian", "chinese", "euro", "happy", "power", "fusion", "free", "speed", "trash", "extreme", "surf", "brit", "emo", "electronic", "neo", "noise", "psychedelic", "post", "conscious", "battle", "east coast", "west coast", "glam", "heavy", "avantgarde", "gothic", "groove", "funk", "nu", "melodic", "celtic", "industrial", "latin", "traditional", "harmonica", "dirty", "gospel", "balkan", "lounge", "rotterdam", "sunshine", "street", "skate", "garage", "horror", "detroit", "ghetto", "acid"]
+city = ["rotterdam", "berlin", "detroit", "gothenburg", "san francisco", "london", "rio"]
+
+subgroup = ["medieval", "death", "black", "hard", "progressive", "symphonic", "viking", "pagan", "gangsta", "experimental", "alternative", "minimal", "dark", "synth", "future", "body", "christian", "indian", "chinese", "euro", "happy", "power", "fusion", "free", "speed", "trash", "extreme", "surf", "brit", "emo", "electronic", "neo", "noise", "psychedelic", "post", "conscious", "battle", "east coast", "west coast", "glam", "heavy", "avantgarde", "gothic", "groove", "funk", "nu", "melodic", "celtic", "industrial", "latin", "traditional", "harmonica", "dirty", "gospel", "balkan", "lounge", "sunshine", "street", "skate", "garage", "horror", "ghetto", "acid"]
+
 group = ["core", "metal", "pop", "punk", "country", "rap", "western", "ambient", "techno", "house", "trance", "dance", "beat", "dubstep", "samba", "chiptune", "jazz", "ska", "soul", "aggrotech", "rave", "oi!", "bluegrass", "hip-hop", "grunge", "rockabilly", "industrial", "blues", "humppa", "reggae", "dancehall", "dub", "schranz"]
 
-rand = randint(0, 1)
-if rand is 0: print choice(subgroup), choice(subgroup), choice(group)
-if rand is 1: print choice(subgroup), choice(group)
+rand1 = randint(0, 1)
+rand2 = randint(0, 9)
+if rand1 is 0:
+    if rand2 is 9:
+        print choice(city),
+    print choice(subgroup), choice(subgroup), choice(group)
+if rand1 is 1: 
+    if rand2 is 9:
+        print choice(city),
+    print choice(subgroup), choice(group)
 
 # vim: set sw=4 tw=0 ts=4 expandtab:
